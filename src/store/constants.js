307e385cccfd0c82c9b1dcb5b17583d7ecdb4cba
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2019-07-27 23:24:15 +0300

    constants

---
+++ src/store/constants.js
@@ -0,0 +1,48 @@
+// temp
+export const USER = {
+	user_id: 1,
+	user_name: 'fatihsinanyaman',
+	user_fullname: 'Fatih Sinan Yaman',
+	user_image: 'https://pbs.twimg.com/profile_images/1154632712197685250/ehckAScZ_400x400.jpg',
+	user_bio: 'Linux, PHP, Javascript, Vuejs, Codeigniter, Angular2, Ionic, Angularjs, Rest, CSS, Opencart, Concrete5, Mysql and bla bla...'
+},
+
+// temp
+export const PROJECT= {
+	project_id: 1,
+	user_id: 1,
+	project_title: 'Wire Bender',
+	project_description: 'I build sculptures out of brass wires and since I hate repeating creating same part over and over again I decided to build a wire bending CNC machine that would do the job for me. So far it is still an early prototype but looks promising! The goal is to build a machine that has most of the parts 3D printer or easy to get so everybody can build it at home.<br><br>All the resources will be available on GitHub <a target="_blank" rel="noopener noreferrer" href="https://github.com/jpraus/wirebender">https://github.com/jpraus/wirebender</a>',
+	project_images: [
+		'https://hackaday.io/project/166369/gallery#2a4751919f61887d64853be5b56333f8',
+		'https://hackaday.io/project/166369/gallery#b1b8e67414b9b6ebb0cc198e392bf495',
+		'https://hackaday.io/project/166369/gallery#e75ec43b89222206c04118bce9d9c2a9'
+	],
+	project_video_url: 'https://youtu.be/U4jRoeQRpb0',
+	project_tags: ['wire', 'bender'],
+	project_category: 'maker',
+	project_files: [],
+};
+
+// temp
+export const PROJECT_LIST = [
+	{
+		project_id: 1,
+		user_id: 1,
+		user_fullname: 'Fatih Sinan Yaman',
+		project_title: 'Wire Bender',
+		project_description: 'I build sculptures out of brass wires and since I hate repeating creating same part over and over again I decided to build a wire bending CNC machine that would do the job for me. So far it is still an early prototype but looks promising! The goal is to build a machine that has most of the parts 3D printer or easy to get so everybody can build it at home.<br><br>All the resources will be available on GitHub <a target="_blank" rel="noopener noreferrer" href="https://github.com/jpraus/wirebender">https://github.com/jpraus/wirebender</a>',
+		project_tags: ['wire', 'bender'],
+		project_category: 'maker',
+	},
+	{
+		project_id: 2,
+		user_id: 2,
+		user_fullname: 'Kubilay Akkurt',
+		project_title: 'Pocket Admin',
+		project_description: 'This is an open source keystroke injection device, similar to a well known USB    rubber ducky made by hak5. It looks and feels like an ordinary USB flash drive   but acts as a keyboard that types in a preprogrammed payload. This can be very      useful for automating sysadmin tasks or in penetration testing applications.     For more information check its github repository:     <a target="_blank" rel="noopener noreferrer" href="https://github.com/krakrukra/PocketAdmin">https://github.com/krakrukra/PocketAdmin</a>',
+		project_tags: ['pocket'],
+		project_category: 'maker',
+		project_image: 'https://hackaday.io/project/165926/gallery#1c4e9f98e5725e4770acf46b796fcb42',
+	},
+];
